c4f9663781202f015d2b8871304a6f4477eb9e63
pjx206 <pjx206@163.com>
2020-02-21 11:07:10 +0800

    issue: cannot display currect time

--- turtleDigitalClock/digitalClock.py
+++ turtleDigitalClock/digitalClock.py
@@ -1,31 +1,30 @@
 import turtle as tt
-from settings import Segments, Numbers
+from settings import Segments, Numbers, Colon
+import time, asyncio
 
-#! Turtle uses a normal cordinate system: (0, 0) is left bottom
-#! points in segments is using a screen cordinate system: (0, 0) is left top
-
+tt.speed(2)
 class DigitalClock:
-    def __init__(self, initTime):
-        """initTime format: hh:mm:ss"""
-        tmp = initTime.split(':')
-        self._hour = tmp[0]
-        self._minute = tmp[1]
-        self._seconf = tmp[2]
+    def __init__(self):
         self._Numbers = Numbers
         self._Segments = Segments
-        tt.isvisible = False
+        self._Colon = Colon
+        tt.speed('slow')
+        tt.hideturtle()
+        tt.penup()
 
-    def __drawSeg(self, seg, leftTop: tuple):
+    def __getAbsolutePositions(self, begin, reletivePositons):
+        return [point for point in map(
+            lambda p:(p[0]+begin[0], p[1]+begin[1]), reletivePositons)]
+
+    def __drawSeg(self, seg, beginPos: tuple):
         """draw one segment of the digit
         param seg: which segment in a digital '8' to draw
         param topleft:a tuple: (x，y)
         """
 
         # calculate absolute position
-        absolutePositions = [point for point in map(
-            lambda p:(p[0]+leftTop[0], p[1]+leftTop[1]), self._Segments[seg])]
+        absolutePositions = self.__getAbsolutePositions(beginPos, self._Segments[seg])
 
-        tt.penup()
         tt.goto(absolutePositions[0])
         tt.pendown()
         tt.begin_fill()
@@ -34,17 +33,51 @@ class DigitalClock:
         tt.end_fill()
         tt.penup()
 
-    def DisplayDigit(self, number, leftTop):
+
+    def __drawDigit(self, number, beginPos: tuple):
         if number < 0 or number > 9:
-            # invalid number
+            # invalid number  
             exit(1)
 
         number = self._Numbers[number]
         for seg in range(len(number)):
             if number[seg] == 1:
-                self.__drawSeg(seg, leftTop)
+                self.__drawSeg(seg, beginPos)
+    
+    def __drawColon(self, beginPos: tuple):
+        for i in range(2):
+            absolutePositions = self.__getAbsolutePositions(beginPos, self._Colon[i])
+            tt.goto(absolutePositions[0])
+            tt.pendown()
+            tt.begin_fill()
+            for point in absolutePositions[1:]:
+                tt.goto(point)
+            tt.end_fill()
+            tt.penup()
 
 
-clock = DigitalClock('1:1:1')
-clock.DisplayDigit(0, (0, 0))
-clock.DisplayDigit(1, (100, 100))
+    def displayTime(self):
+        now = time.strftime("%H:%M", time.localtime()).split(':')
+        # draw hour
+        tt.color('#298FEF')
+        self.__drawDigit(int(now[0][0]), (-42 * 2, 0))
+        self.__drawDigit(int(now[0][1]), (-42 * 1, 0))
+        tt.color('#b2cf87')
+        self.__drawColon((0, 0))
+        tt.color('#298FEF')
+        self.__drawDigit(int(now[1][0]), (10, 0))
+        self.__drawDigit(int(now[1][0]), (50, 0))
+
+async def displayClock():
+    clock = DigitalClock()
+    # loop = asyncio.get_running_loop()
+    # end_time = loop.time() + 60.0 * 10 # 10 mins
+    while True:
+        clock.displayTime()
+        # if(loop.time() + 1.0) >= end_time:
+        #     break
+        tt.clear()
+        await asyncio.sleep(8)
+
+if __name__ == '__main__':
+    asyncio.run(displayClock())
